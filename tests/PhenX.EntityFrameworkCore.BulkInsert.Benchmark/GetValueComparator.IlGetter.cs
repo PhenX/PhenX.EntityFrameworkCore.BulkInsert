c0e0c0ef4f74c65d3a31e895c7a368144d291554
Fabien Ménager <PhenX@users.noreply.github.com>
2025-06-01 16:05:20 +0200

    Feature/improve allocations (#49)
    * Improve memory allocation thanks to expression trees
    
    * Update benchmark results
    
    * Add benchmark for getters
    
    * Native benchmark more accurate
    
    * Improve naming

---
+++ tests/PhenX.EntityFrameworkCore.BulkInsert.Benchmark/GetValueComparator.IlGetter.cs
@@ -0,0 +1,64 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+
+namespace PhenX.EntityFrameworkCore.BulkInsert.Benchmark;
+
+public partial class GetValueComparator
+{
+    public static Func<object, object?> CreateUntypedGetter(PropertyInfo propertyInfo, Type sourceType, Type valueType)
+    {
+        var method =
+            typeof(GetValueComparator).GetMethod(nameof(CreateInternalUntypedGetter), BindingFlags.NonPublic | BindingFlags.Static)!
+                .MakeGenericMethod(sourceType, valueType);
+
+        return (Func<object, object?>)method.Invoke(null, [propertyInfo])!;
+    }
+
+    private static Func<object, object?> CreateInternalUntypedGetter<TSource, TValue>(PropertyInfo propertyInfo)
+    {
+        var getter = CreateGetter<TSource, TValue>(propertyInfo);
+
+        return source => getter((TSource)source!);
+    }
+
+    public static Func<TSource, TValue> CreateGetter<TSource, TValue>(PropertyInfo propertyInfo)
+    {
+        if (!propertyInfo.CanRead)
+        {
+            return x => throw new NotSupportedException();
+        }
+
+        var bakingField =
+            propertyInfo.DeclaringType!.GetField($"<{propertyInfo.Name}>k__BackingField",
+                BindingFlags.NonPublic |
+                BindingFlags.Instance);
+
+        var propertyGetMethod = propertyInfo.GetGetMethod()!;
+
+        var getMethod = new DynamicMethod(propertyGetMethod.Name, typeof(TValue), [typeof(TSource)], true);
+        var getGenerator = getMethod.GetILGenerator();
+
+        // Load this to stack.
+        getGenerator.Emit(OpCodes.Ldarg_0);
+
+        if (bakingField != null && !propertyGetMethod.IsVirtual)
+        {
+            // Get field directly.
+            getGenerator.Emit(OpCodes.Ldfld, bakingField);
+        }
+        else if (propertyGetMethod.IsVirtual)
+        {
+            // Call the virtual property.
+            getGenerator.Emit(OpCodes.Callvirt, propertyGetMethod);
+        }
+        else
+        {
+            // Call the non virtual property.
+            getGenerator.Emit(OpCodes.Call, propertyGetMethod);
+        }
+
+        getGenerator.Emit(OpCodes.Ret);
+
+        return getMethod.CreateDelegate<Func<TSource, TValue>>();
+    }
+}
---
+++ tests/PhenX.EntityFrameworkCore.BulkInsert.Benchmark/GetValueComparator.cs
@@ -0,0 +1,149 @@
+using System.Linq.Expressions;
+using System.Reflection;
+
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Engines;
+
+using PhenX.EntityFrameworkCore.BulkInsert.Metadata;
+
+namespace PhenX.EntityFrameworkCore.BulkInsert.Benchmark;
+
+[MemoryDiagnoser]
+[SimpleJob(RunStrategy.ColdStart, launchCount: 1, warmupCount: 0, iterationCount: 10)]
+public partial class GetValueComparator
+{
+    [Params(1_000_000)] public int N;
+
+    private IReadOnlyList<TestEntity> data = [];
+
+    [IterationSetup]
+    public void IterationSetup()
+    {
+        data = Enumerable.Range(1, N).Select(i => new TestEntity
+        {
+            Name = $"Entity{i}",
+            Price = (decimal)(i * 0.1),
+            Identifier = Guid.NewGuid(),
+            NumericEnumValue = (NumericEnum)(i % 2),
+        }).ToList();
+    }
+
+    private static Dictionary<string, Expression<Func<object?, object?>>> Converters = new()
+    {
+        { nameof(TestEntity.NumericEnumValue), v => (int) v},
+    };
+
+    private static readonly PropertyInfo[] PropertyInfos = typeof(TestEntity).GetProperties();
+
+    private static readonly Func<object, object?>[] PropertyInfoGetValueGetters = PropertyInfos
+        .Select<PropertyInfo, Func<object, object?>>(propertyInfo =>
+        {
+            var converter = Converters.TryGetValue(propertyInfo.Name, out var expression)
+                ? expression.Compile()
+                : null;
+
+            if (converter == null)
+            {
+                return propertyInfo.GetValue;
+            }
+
+            return entity => converter(propertyInfo.GetValue(entity));
+        })
+        .ToArray();
+
+    private static readonly Func<object, object?>[] PropertyInfoIlGetters = PropertyInfos
+        .Select<PropertyInfo, Func<object, object?>>(propertyInfo =>
+        {
+            var converter = Converters.TryGetValue(propertyInfo.Name, out var expression)
+                ? expression.Compile()
+                : null;
+
+            var getter = CreateUntypedGetter(propertyInfo, propertyInfo.DeclaringType!, propertyInfo.PropertyType);
+
+            if (converter == null)
+            {
+                return getter;
+            }
+
+            return entity => converter(getter(entity));
+        })
+        .ToArray();
+
+    private static readonly Func<object, object?>[] PropertyAccessorGetters = PropertyInfos
+        .Select<PropertyInfo, Func<object, object?>>(propertyInfo =>
+        {
+            var converter = Converters.TryGetValue(propertyInfo.Name, out var expression)
+                ? expression
+                : null;
+
+            return PropertyAccessor.CreateGetter(propertyInfo, converter);
+        })
+        .ToArray();
+
+    [Benchmark(Baseline = true)]
+    public void Native()
+    {
+        var enumConverter = Converters[nameof(TestEntity.NumericEnumValue)].Compile();
+
+        for (var i = 0; i < data.Count; i++)
+        {
+            var entity = data[i];
+
+            _ = entity.Id;
+            _ = entity.Name;
+            _ = entity.Price;
+            _ = entity.Identifier;
+            _ = enumConverter(entity.NumericEnumValue);
+            _ = entity.CreatedAt;
+            _ = entity.UpdatedAt;
+        }
+    }
+
+    [Benchmark]
+    public void PropertyInfo_GetValue()
+    {
+        for (var i = 0; i < data.Count; i++)
+        {
+            var entity = data[i];
+
+            for (var j = 0; j < PropertyInfoGetValueGetters.Length; j++)
+            {
+                var getter = PropertyInfoGetValueGetters[j];
+
+                _ = getter(entity);
+            }
+        }
+    }
+
+    [Benchmark]
+    public void ExpressionTreeGetter()
+    {
+        for (var i = 0; i < data.Count; i++)
+        {
+            var entity = data[i];
+
+            for (var j = 0; j < PropertyAccessorGetters.Length; j++)
+            {
+                var getter = PropertyAccessorGetters[j];
+
+                _ = getter(entity);
+            }
+        }
+    }
+
+    [Benchmark]
+    public void IlGetter()
+    {
+        for (var i = 0; i < data.Count; i++)
+        {
+            var entity = data[i];
+
+            for (var j = 0; j < PropertyInfoIlGetters.Length; j++)
+            {
+                var getter = PropertyInfoIlGetters[j];
+
+                _ = getter(entity);
+            }
+        }
+    }
+}

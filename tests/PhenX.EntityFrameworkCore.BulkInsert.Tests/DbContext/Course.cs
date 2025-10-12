f9c4938982a69dcc691caa463923a4e5799d8fd1
fabien.menager <fabien.menager@am-creations.fr>
2025-10-12 11:29:45 +0200

    Fixes #63 : cannot handle shadow properties, or properties without getter, or properties with a getter with arguments

---
+++ tests/PhenX.EntityFrameworkCore.BulkInsert.Tests/DbContext/Course.cs
@@ -0,0 +1,8 @@
+﻿namespace PhenX.EntityFrameworkCore.BulkInsert.Tests.DbContext;
+
+public class Course
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public ICollection<Student> Students { get; set; } = new List<Student>();
+}
---
+++ tests/PhenX.EntityFrameworkCore.BulkInsert.Tests/DbContext/Student.cs
@@ -0,0 +1,8 @@
+﻿namespace PhenX.EntityFrameworkCore.BulkInsert.Tests.DbContext;
+
+public class Student
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public ICollection<Course> Courses { get; set; } = new List<Course>();
+}

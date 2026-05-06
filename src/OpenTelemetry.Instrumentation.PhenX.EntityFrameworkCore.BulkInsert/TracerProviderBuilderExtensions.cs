993fb6e3854875e19517d023f81dafaace6d4b78
Pschichholz <david.pschichholz@dedalus.eu>
2026-05-06 16:27:15 +0200

    Add OpenTelemetry instrumentation project and update README

---
+++ src/OpenTelemetry.Instrumentation.PhenX.EntityFrameworkCore.BulkInsert/TracerProviderBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using OpenTelemetry.Trace;
+namespace OpenTelemetry.Instrumentation.PhenX.EntityFrameworkCore.BulkInsert;
+
+/// <summary>
+/// Extension methods for <see cref="TracerProviderBuilder"/> to add instrumentation for PhenX.EntityFrameworkCore.BulkInsert.
+/// </summary>
+public static class TracerProviderBuilderExtensions
+{
+    /// <summary>
+    /// Adds instrumentation for PhenX.EntityFrameworkCore.BulkInsert to the OpenTelemetry TracerProviderBuilder.
+    /// </summary>
+    /// <param name="builder">The TracerProviderBuilder to add the instrumentation to.</param>
+    /// <returns>The TracerProviderBuilder with the PhenX.EntityFrameworkCore.BulkInsert instrumentation added.</returns>
+    public static TracerProviderBuilder AddPhenXEntityFrameworkCoreBulkInsertInstrumentation(this TracerProviderBuilder builder)
+    {
+        return builder.AddSource("PhenX.EntityFrameworkCore.BulkInsert");
+    }
+}
